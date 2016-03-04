cc54e72651b4972ac06dcc86093c41fc0a39a137
erinthered <erinbesswilliams@gmail.com>
2016-03-03 19:22:17 -0500

    Added task 1

--- Lab5.cpp
+++ Lab5.cpp
@@ -5,7 +5,7 @@ Instructor: Ilya Korsunskiy
 Assignment: Lab 5
 
 This program ...
-*/
+ */
 
 #include <iostream>
 #include <cmath>
@@ -15,53 +15,65 @@ using namespace std;
 // Postcondition: the return value is num read backwards (in decimal)
 int reversePos(int num);
 
-int numDigits(int num);            // return the number of digits in num
+// Precondition: num > 0
+// Postcondition: the return value is the number of digits in num
+int numDigits(int num);
 
-int getDigit(int num, int index);  // return the index'th digit of num
+// Precondition: num > 0, index > 0
+// Postcondition: returns the digit of num located at index
+int getDigit(int num, int index);
+
 
 int main()
 {
-int num;
+	int n;
 
-cout << "Enter a positive number: ";
-cin >> num;
+	cout << "Enter a positive number: ";
+	cin >> n;
 
-cout << reversePos(num) << endl;
+	// Precondition: num > 0
+	// Postcondition: Outputs a table of multiples of num up to 1023 and their reverses
+	int multiple = n;
+	for (int i=1; i<=1023 && multiple<=1023; i++)
+	{
+		multiple = n*i;
+		cout << multiple << "\t" << reversePos(multiple) << endl;
+	}
 
 return 0;
 }
 
 int numDigits(int number)
 {
-    int counter = 0;
-    for (int i=number; i>0; i/=10)
-    {
-        counter++;
-    }
-    return counter;
+	int counter = 0;
+	for (int i=number; i>0; i/=10)
+	{
+		counter++;
+	}
+	return counter;
 }
 
 int getDigit(int num, int index)
 {
-    int numAtIndex = 0;
-    for (int i=0; i<index; i++)
-    {
-        num /= 10;
-    }
-    numAtIndex = num % 10; 
-    return numAtIndex;
+	int numAtIndex = 0;
+	for (int i=0; i<index; i++)
+	{
+		num /= 10;
+	}
+	numAtIndex = num % 10; 
+	return numAtIndex;
 }
 
 int reversePos(int num)
 {
-    int numDig = numDigits(num);
-    cout << "Number of digits: " << numDig << endl;
-    int sum = 0;
-    for (int i=0; i<numDig; i++)
-    {
-       int numAtIndex = getDigit(num, i);
-       int reverseNum = numAtIndex * (pow(10, numDig-(i+1)));
-       sum += reverseNum;
-    }
-    return sum;
+	int numDig = numDigits(num);
+	cout << "Number of digits: " << numDig << endl;
+	int sum = 0;
+	for (int i=0; i<numDig; i++)
+	{
+		int numAtIndex = getDigit(num, i);
+		int reverseNum = numAtIndex * (pow(10, numDig-(i+1)));
+		sum += reverseNum;
+	}
+	return sum;
 }
