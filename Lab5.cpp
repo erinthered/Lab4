c4d53cb551995de0a80cee015bf2a501c2ee6b03
erinthered <erinbesswilliams@gmail.com>
2016-03-09 14:19:46 -0500

    added comments & test cases for final

--- Lab5.cpp
+++ Lab5.cpp
@@ -4,10 +4,13 @@ Course: 136
 Instructor: Ilya Korsunskiy
 Assignment: Lab 5
 
-This program ...
+This program takes an integer as input and outputs a table of values of all
+multiples of the number between -1023 and 1023 inclusive in the first column
+and the reverse of the multiple in the second column.
  */
 
 #include <iostream>
+#include <cstdlib>
 #include <cmath>
 using namespace std;
 
@@ -23,24 +26,98 @@ int numDigits(int num);
 // Postcondition: returns the digit of num located at index
 int getDigit(int num, int index);
 
+// Precondition: null
+// Postconditon: the return value is num reversed, with same sign as original num
+int reverse(int num); 
+
+// Precondition: variable n exists outside of function
+// Postcondition: the value of n is the reverse of n
+void reverseInPlace(int &n);
 
 int main()
 {
+	// Driver program for Tasks 1 and 2 (rewritten per instructions in Task 2
+	// to handle the range of multiples of n between -1023-1023 inclusive).
+        // Tested on a range of values including both ends of the range, 0, and +-1
 	int n;
+	char ans;
 
-	cout << "Enter a positive number: ";
-	cin >> n;
-
-	// Precondition: num > 0
-	// Postcondition: Outputs a table of multiples of num up to 1023 and their reverses
-	int multiple = n;
-	for (int i=1; i<=1023 && multiple<=1023; i++)
+	do
 	{
-		multiple = n*i;
-		cout << multiple << "\t" << reversePos(multiple) << endl;
-	}
+		// Get integer input from user
+		cout << "Enter an integer: ";
+		cin >> n;
 
-return 0;
+                // Handles special case of n=0, which causes infinite loop
+		if (n==0)
+		{
+			cout << n << "\t" << n << endl;
+		}
+		else
+		{
+			int multiple = n;
+			for (int i=-1023; i<=1023; i++)
+			{
+				multiple = n*i;
+
+				if (multiple<=1023 && multiple>=-1023)  // Range between -1023 and 1023
+				{
+					if (n<0)
+					{
+						// Requirements were to print a table between -1023 to 1023, so this
+                                                // lists neg multiples first for neg num, then counts up toward pos. 
+						multiple = multiple * -1;  
+					}
+					cout << multiple << "\t" << reverse(multiple) << endl;
+				}
+			}
+		} 
+		cout << "Do you want to test again? (y/n)";
+		cin >> ans;
+		cout << endl;
+	} while (ans == 'y' || ans == 'Y');
+
+	// Driver program for Task 3, producing a table of multiples of the input 
+	// number n, between -1023 and 1023 inclusive
+        // Tested on a range of values including both ends of the range, 0, and +-1
+	do
+	{
+		cout << "Enter an integer: ";
+		cin >> n;
+
+                // Handles special case of n=0, which causes infinite loop
+		if (n==0)
+		{
+			cout << n << "\t" << n << endl;
+		}
+		else
+		{
+			int multiple = n;
+			for (int i=-1023; i<1023; i++)  // Range between -1023 and 1023
+			{
+				multiple = n*i;
+
+				if (multiple<=1023 && multiple>=-1023)
+				{
+					if (n<0)
+					{
+                                                // Requirements were to print a table between -1023 to 1023, so this
+                                                // lists neg multiples first for neg num, then counts up toward pos.
+						multiple = multiple * -1;
+					}
+              
+					cout << multiple << "\t";  // Print original multiple result
+					reverseInPlace(multiple);  // Reverse value of multiple
+					cout << multiple << endl;  // Print reverse of multiple
+				}
+			}
+		}
+		cout << "Do you want to test again? (y/n)";
+		cin >> ans;
+		cout << endl;
+	} while (ans == 'y' || ans == 'Y');
+
+	return 0;
 }
 
 int numDigits(int number)
@@ -67,7 +144,6 @@ int getDigit(int num, int index)
 int reversePos(int num)
 {
 	int numDig = numDigits(num);
-	cout << "Number of digits: " << numDig << endl;
 	int sum = 0;
 	for (int i=0; i<numDig; i++)
 	{
@@ -76,4 +152,21 @@ int reversePos(int num)
 		sum += reverseNum;
 	}
 	return sum;
+}
+
+int reverse(int num)
+{
+	int absNum = abs(num);
+	int revNum = reversePos(absNum);
+	if (num < 0)
+	{
+		revNum = revNum * (-1);
+	}
+	return revNum;
+}
+
+void reverseInPlace(int& n)
+{
+	n = reverse(n);
+	return;
 }
